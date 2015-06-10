41b9bcf492e27494ea83f000869762242bfe4b42
Ryan <ryan@rotogrinders.com>
2015-06-10 16:01:04 -0500

    This solution isn't much better, but I thought that I could use some cool concurrency stuff. Will try again later with channels.

--- problem5.go
+++ problem5.go
@@ -4,19 +4,21 @@ package main
 import "fmt"
 
 func main() {
-	for i := 20; true; i++ {
-		if isDivisible(i) {
-			fmt.Println(i)
-			break
-		}
+	cont := true
+	for i := 20; cont; i++ {
+		// I tried adding 'go' here and the program took far longer. I'm wondering if
+		// maybe I need channels to do this right?
+		isDivisible(i, &cont)
 	}
 }
 
-func isDivisible(p int) bool {
+func isDivisible(num int, cont *bool) bool {
 	for i := 1; i < 21; i++ {
-		if p%i != 0 {
+		if num%i != 0 {
 			return false
 		}
 	}
+	fmt.Println(num)
+	*cont = false
 	return true
 }
